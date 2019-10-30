0e4d05ea37b9a61ab2a8a43d1eaf0b146f2c629e
anomeme <x.music.sky@gmail.com>
2019-10-30 17:44:31 +0900

    view-create_first-commito

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,16 @@
+class SignupController < ApplicationController
+  def form1
+  end
+
+  def form2
+  end
+
+  def form3
+  end
+
+  def form4
+  end
+
+  def complete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#index'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    collection do
+      get 'signup/form1'
+      get 'signup/form2'
+      get 'signup/form3'
+      get 'signup/form4'
+      get 'signup/complete'
+    end
+  end
 end
