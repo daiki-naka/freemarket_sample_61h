fdf752644b9420d66e821c33206b56859fd498f5
daiki-naka <daught.1994@icloud.com>
2019-11-02 16:00:37 +0900

    retouching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
+  resources :pages, only: [:index, :show]
 
   # resources入れる
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
