d5a7b7adfd7470ca4cb471370e6284148d238c7e
Sunny-arren <cysctoqv25lp@gmail.com>
2019-11-04 11:18:36 +0900

    headerとfooter マークアップ提出（一次）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'products#index'
-
-  # resources入れる
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'products#footer'
+  resources :products
+  resources :users
 end
