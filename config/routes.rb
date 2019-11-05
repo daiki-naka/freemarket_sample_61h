4db9b63332cdd5ca183b0a5e7e5850538caab5db
daiki-naka <daught.1994@icloud.com>
2019-11-05 14:00:39 +0900

    retouching-create-user-creditcard-save

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   resources :products
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit] do
+    collection do
+      get 'card'
+    end
+  end
   resources :pages, only: [:index]
 end
