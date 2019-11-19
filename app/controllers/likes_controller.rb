5eccf44e1f3913ebfe65c873d31880c836bbc4d3
Sunny-arren <cysctoqv25lp@gmail.com>
2019-11-19 17:05:49 +0900

    指摘された箇所を修正しました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,26 +1,5 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!, only:[:create, :destory]
-#   def create
-#   @records = Likes.where(user_id: current_user.id)
-#   @current_product_record = @records.find_by(product_id: @product)
-#   if !@product.user_id == current_user.id && @current_product_record ?
-#     = render partial: 'like_form_1', locals: { group: @group} 
-#   elsif !@product.user_id == current_user.id && ！@current_product_record ?
-#     = render partial: 'like_form_2'
-#   else
-#     # 処理なし
-#  end
-
-
-# def add_like(user)
-#   likes.create(user_id: user.id)
-# end
-
-# def remove_like(user)
-#   likes.find_by(user_id: user.id).destroy
-# end
-
-
 def create
   @product = Product.find(params[:product_id])
   if !@product.add_like(current_user)
@@ -46,18 +25,3 @@ def destroy
   end
 end
 end
-
-
-
-
-#  def destroy
-#   @like = Like.destroy(params[:product_id, user_id: current_user.id])
-#   if @like.destroy
-#     respond_to do |format|
-#       format.json
-#  end
-
-#  def likes_params
-#   params.require(:like).permit(:product_id, :user_id)
-#  end
-# end
