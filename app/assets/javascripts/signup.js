b654a763a777df24e5b6f171aa8646fe0305cda4
anomeme <x.music.sky@gmail.com>
2019-11-20 15:48:05 +0900

    signup 微調整

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function(){
 
-  var progress = 0;
-  var pageCount = 8;
-  $('.signup-form__container__inner__submit').each('submit', function(){
-  })
+  $('.signup-form__container__inner__form__select__prefecture').on('change', function(){
+    $('.signup-form__container__inner__form__select__prefecture').css('color', 'black')
+  });
+  
 });
