b4e5444deabfc505a7e9a8ca36fdadb053ded7cb
Sunny-arren <cysctoqv25lp@gmail.com>
2019-11-22 01:14:50 +0900

    商品詳細ページと、出品者の商品編集ページトップの画像スライダー修正

--- app/assets/javascripts/product_exhibit-image.js
+++ app/assets/javascripts/product_exhibit-image.js
@@ -5,21 +5,33 @@ $(document).on('turbolinks:load',function(){
     $('.item__main-content__photo__detail__stage li:eq(2)').css("left","600px");
     $('.item__main-content__photo__detail__stage li:eq(3)').css("left","900px");
     $('.item__main-content__photo__detail__stage li:eq(4)').css("left","1200px");
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","1.0");
   });
   
-  $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(0)",function (){
+  $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(0)",function () {
     $('.item__main-content__photo__detail__stage').animate({"left": "0px"});
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","1.0");
   });
-  $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(1)",function (){
+  $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(1)",function () {
     $('.item__main-content__photo__detail__stage').animate({"left":"-300px"});
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","0.4");
   });
   $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(2)",function(){
     $('.item__main-content__photo__detail__stage').animate({"left":"-600px"});
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","0.4");
   });
   $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(3)",function(){
     $('.item__main-content__photo__detail__stage').animate({"left":"-900px"});
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","0.4");
   });
   $(document).on("mouseenter",".item__main-content__photo__details__dot:eq(4)",function(){
     $('.item__main-content__photo__detail__stage').animate({"left":"-1200px"});
+    $('.item__main-content__photo__details__dot__image:eq(0)').css("opacity","0.4");
   });
 });
+
+
+
+
+
+
